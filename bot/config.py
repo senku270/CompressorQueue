3d7d8e5ecc2773164302d84d6747da39ec9bdf86
senku270 <145897244+senku270@users.noreply.github.com>
2025-05-21 15:21:15 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,14 +16,14 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = config("APP_ID", "4018758")
+    API_HASH = config("API_HASH", "622bba3cf046315531f71f9d97fa6c2a")
+    BOT_TOKEN = config("BOT_TOKEN", "6942815361:AAHwpmE7ThoaKWJJyRIrIZHRXOaxooCXGd8")
     DEV = 1322549723
-    OWNER = config("OWNER")
+    OWNER = config("OWNER", "5385471287")
     FFMPEG = config(
         "FFMPEG",
-        default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
+        default='ffmpeg -i "{}" -preset veryfast -c:v libx265 -crf 27 -b:a 40k  -s 854x480 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
     )
     TELEGRAPH_API = config("TELEGRAPH_API", default="https://api.telegra.ph")
     THUMB = config(
